e88f9acbdd996d27969aa5ebca2500078790e61b
Abdelbasset <abagga@elyosdigital.com>
2020-01-09 18:03:31 +0100

    add login component & services & helpers

---
+++ src/app/_services/auth.service.ts
@@ -0,0 +1,83 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap, mapTo, catchError } from 'rxjs/operators';
+import { of, Observable } from 'rxjs';
+import { config } from '../apiConfig';
+import { Tokens } from '../_models/tokens';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private readonly JWT_TOKEN = 'JWT_TOKEN';
+  private readonly REFRESH_TOKEN = 'REFRESH_TOKEN';
+  private loggedUser: string;
+
+  constructor(private http: HttpClient) { }
+
+  login(user: { username: string, password: string }): Observable<boolean> {
+    return this.http.post<any>(`${config.apiUrl}/login`, user)
+      .pipe(
+        tap(tokens => this.doLoginUser(user.username, tokens)),
+        mapTo(true),
+        catchError(error => {
+          alert(error.error);
+          return of(false);
+        }));
+  }
+
+  logout() {
+    return this.http.post<any>(`${config.apiUrl}/logout`, {
+      'refreshToken': this.getRefreshToken()
+    }).pipe(
+      tap(() => this.doLogoutUser()),
+      mapTo(true),
+      catchError(error => {
+        alert(error.error);
+        return of(false);
+      }));
+  }
+  refreshToken() {
+    return this.http.post<any>(`${config.apiUrl}/refresh`, {
+      'refreshToken': this.getRefreshToken()
+    }).pipe(tap((tokens: Tokens) => {
+      this.storeJwtToken(tokens.jwt);
+    }));
+  }
+
+  getJwtToken() {
+    return localStorage.getItem(this.JWT_TOKEN);
+  }
+
+  isLoggedIn() {
+    return !!this.getJwtToken();
+  }
+  
+  private doLoginUser(username: string, tokens: Tokens) {
+    this.loggedUser = username;
+    this.storeTokens(tokens);
+  }
+
+  private doLogoutUser() {
+    this.loggedUser = null;
+    this.removeTokens();
+  }
+
+  private getRefreshToken() {
+    return localStorage.getItem(this.REFRESH_TOKEN);
+  }
+
+  private storeJwtToken(jwt: string) {
+    localStorage.setItem(this.JWT_TOKEN, jwt);
+  }
+
+  private storeTokens(tokens: Tokens) {
+    localStorage.setItem(this.JWT_TOKEN, tokens.jwt);
+    localStorage.setItem(this.REFRESH_TOKEN, tokens.refreshToken);
+  }
+
+  private removeTokens() {
+    localStorage.removeItem(this.JWT_TOKEN);
+    localStorage.removeItem(this.REFRESH_TOKEN);
+  }
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { AuthGuard } from './_helpers/auth.guard';
+import { RandomGuard } from './_helpers/random.guard';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/login' },
+  
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [AuthGuard]
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
